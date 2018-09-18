35cd6e9aa4b618748f5902f603dc5ba6df74ddb9
Jan Narkiewicz <jdnark@gmail.com>
2018-09-18 10:54:09 -0700

    added 7

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World from RB test6\n";
+  cout << "Hello, World from RB test7\n";
   cout << "Hello, Jenkins\n";
 
   cout << "I have successfully built and run\n";
